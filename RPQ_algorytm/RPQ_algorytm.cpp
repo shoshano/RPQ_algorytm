32dfb2e852e57ee6e8d3f9a7f8aca36057cfad1c
Maciek Danik <maciekdanik@gmail.com>
2024-03-13 18:22:01 +0100

    sortR

--- RPQ_algorytm/RPQ_algorytm.cpp
+++ RPQ_algorytm/RPQ_algorytm.cpp
@@ -3,105 +3,102 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <algorithm>
 
 using namespace std;
 
-
-
 class RPQ {
 public:
-    int index;
-    int R;
-    int P;
-    int Q;
+	int index;
+	int R;
+	int P;
+	int Q;
 
-    RPQ(int r, int p, int q){
-        this->R = r;
-        this->P = p;
-        this->Q = q;
-        this->index = 1;
-    }
+	RPQ(int r, int p, int q) {
+		this->R = r;
+		this->P = p;
+		this->Q = q;
+		this->index = 1;
+	}
 };
 ostream& operator<<(ostream& out, const RPQ& d)
 {
-    return out << "ID: " << d.index << "\tR: " << d.R << "\tP : " << d.P << "\tQ : " << d.Q << endl;
+	return out << "ID: " << d.index << "\tR: " << d.R << "\tP : " << d.P << "\tQ : " << d.Q << endl;
 }
 
-
 class Dane {
+	vector<RPQ> lista;
+	int number_of_data;
+	string file_path;
 
-    vector<RPQ> lista;
-    int number_of_data;
-    string file_path;
+public:
+	Dane(string fp) {
+		file_path = fp;
+		GetData();
+	}
 
-public: 
-    Dane(string fp){
-        file_path = fp;
-        GetData();
-    }
+	void GetData() {
+		number_of_data = 0;
+		string n;
+		fstream file;
+		file.open(file_path);
+		string line, chunk;
+		vector<string> tmp;
+		if (file.is_open()) {
+			getline(file, line);
+			number_of_data = stoi(line);
+			for (int i = 0; i < number_of_data; i++) {
+				RPQ d = RPQ(0, 0, 0);
+				getline(file, line);
+				stringstream s(line);
+				while (getline(s, chunk, ' '))
+				{
+					tmp.push_back(chunk);
+				}
+				d.index = i + 1;
+				d.R = stoi(tmp.at(0));
+				d.P = stoi(tmp.at(1));
+				d.Q = stoi(tmp.at(2));
 
-    void GetData() {
-        number_of_data = 0;
-        string n;
-        fstream file;
-        file.open(file_path);
-        string line, chunk;
-        vector<string> tmp;
-        if (file.is_open()) {
-            getline(file, line);
-            number_of_data = stoi(line);
-            for (int i = 0; i < number_of_data; i++) {
-                RPQ d = RPQ(0, 0, 0);
-                getline(file, line);
-                stringstream s(line);
-                while (getline(s, chunk, ' '))
-                {
-                    tmp.push_back(chunk);
-                }
-                d.index = i + 1;
-                d.R = stoi(tmp.at(0));
-                d.P = stoi(tmp.at(1));
-                d.Q = stoi(tmp.at(2));
+				lista.push_back(d);
+				tmp.erase(tmp.begin(), tmp.end());
+			}
+		}
+		else {
+			cout << "Plik nie otwarty";
+		}
+	}
 
-                lista.push_back(d);
-                tmp.erase(tmp.begin(), tmp.end());
-            }
-        }
-        else {
-            cout << "Plik nie otwarty";
-        }
-    }
+	void printDane() {
+		cout << "NR: " << number_of_data << endl;
+		for (auto r : lista)
+		{
+			cout << r;
+		}
+	}
 
-    void printDane() {
-        cout << "NR: " << number_of_data << endl;
-        for(auto r : lista)
-        {
-            cout << r;
-        }
-    }
+	vector<int> sortR(vector<RPQ> lista)
+	{
+	}
 };
-
 
 int main()
 {
-    
-    Dane data1 = Dane("dane1.txt");
-    data1.printDane();
-    
-    cout << "=======================================\n";
+	Dane data1 = Dane("dane1.txt");
+	data1.printDane();
 
-    Dane data2 = Dane("dane2.txt");
-    data2.printDane();
+	cout << "=======================================\n";
 
-    cout << "=======================================\n";
+	Dane data2 = Dane("dane2.txt");
+	data2.printDane();
 
-    Dane data3 = Dane("dane3.txt");
-    data3.printDane();
+	cout << "=======================================\n";
 
-    cout << "=======================================\n";
+	Dane data3 = Dane("dane3.txt");
+	data3.printDane();
 
-    Dane data4 = Dane("dane4.txt");
-    data4.printDane();
+	cout << "=======================================\n";
 
-
+	Dane data4 = Dane("dane4.txt");
+	data4.printDane();
 }
